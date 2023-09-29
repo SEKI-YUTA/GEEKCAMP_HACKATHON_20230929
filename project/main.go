f30a259258a97a584c8631b6fedbc28d374961ca
SEKI-YUA <zard.since1991@gmail.com>
2023-09-29 21:22:42 +0900

    レスポンスを返す処理を関数に切り出し

--- project/main.go
+++ project/main.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	// "os"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router := gin.Default()
+	router.GET("/restaurants", responseAllRestaurants)
+
+	router.Run()
+}
+
+func responseAllRestaurants(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{
+		"message": "Hello, World!",
 	})
-	r.Run()
 }

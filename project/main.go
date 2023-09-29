88a59b0816a41d7c75ff76da0c54fd388ea05c30
SEKI-YUA <zard.since1991@gmail.com>
2023-09-29 20:58:48 +0900

    簡単なginのサーバーを追加

--- project/main.go
+++ project/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }

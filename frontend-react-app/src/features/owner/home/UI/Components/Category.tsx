fa5d1bc71dce42e62b7fbbd1e0e9accc6bf88b07
yuto ichinose <yuto.0526.sano@gmail.com>
2023-10-01 19:39:39 +0900

    fix: スタイルの外部化

--- frontend-react-app/src/features/owner/home/UI/Components/Category.tsx
+++ frontend-react-app/src/features/owner/home/UI/Components/Category.tsx
@@ -11,18 +11,12 @@ export const Category: FC<CategoryProps> = ({name, selected, onPress}) => {
   return (
     <Button
       fontSize={25}
-      style={styles.button}
+      backgroundColor='white'
+      fontWeight='normal'
+      borderRadius='10px'
       // 選択されていたら赤くする
       color={selected ? 'red' : 'black'}
       onClick={onPress}
     >{name}</Button>
   );
-};
-
-const styles = {
-  button: {
-    backgroundColor: 'white',
-    fontWeight: 'normal',
-    borderRadius: '10px',
-  }
 };

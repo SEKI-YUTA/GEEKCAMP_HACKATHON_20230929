6222c055f0481b142786aa2b9953e112550284c5
夏純心 <junshin@XiaTianMacBook-Air.local>
2023-10-02 21:12:20 +0900

    feat:メニュー登録モーダルの追加

--- frontend-react-app/src/features/owner/home/UI/Components/AddMenuModal.tsx
+++ frontend-react-app/src/features/owner/home/UI/Components/AddMenuModal.tsx
@@ -1,4 +1,4 @@
-import { Box, ModalContent, ModalOverlay, Modal, ModalBody, ModalHeader, ModalCloseButton, ModalFooter, Button, FormLabel, Input } from '@chakra-ui/react';
+import { Box, ModalContent, ModalOverlay, Modal, ModalBody, ModalHeader, ModalCloseButton, ModalFooter, Button, FormLabel, Input, Textarea, Flex } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 interface AddMenuModalProps {
@@ -20,9 +20,23 @@ export const AddMenuModal: FC<AddMenuModalProps> = ({
           <ModalHeader>メニュー登録</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Box>
+            <Box py={2}>
               <FormLabel htmlFor='menuName'>メニュー名</FormLabel>
               <Input id='menuName' type='text' />
+            </Box>
+            <Box py={2}>
+              <FormLabel htmlFor='menuPrice'>価格</FormLabel>
+              <Flex alignItems={"center"} gap={3}>
+              <Input id='menuPrice' type='number' />円
+              </Flex>
+            </Box>
+            <Box py={2}>
+              <FormLabel htmlFor='menuDetail'>詳細</FormLabel>
+              <Textarea id='menuDetail'/>
+            </Box>
+            <Box py={2}>
+              <FormLabel htmlFor='imgLink'>メニューの画像リンク</FormLabel>
+              <Input id='imgLink' type='url'/>
             </Box>
           </ModalBody>
           <ModalFooter justifyContent='center'>

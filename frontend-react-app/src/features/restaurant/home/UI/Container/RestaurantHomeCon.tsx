e7658fa344a36b30dbc53d762dbac575a9007723
takeki <takekidanmkk@gmail.com>
2023-10-03 20:18:04 +0900

    add new files

---
+++ frontend-react-app/src/features/restaurant/home/UI/Container/RestaurantHomeCon.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+import { RestaurantHomePre } from '../Presentational/RestaurantHomePre';
+
+/**
+ * レストラン, ホーム画面のコンポーネント（Container）
+ * ここにコンポーネントのロジックを書いて、RestaurantHomePreに渡す
+ */
+
+export const RestaurantHomeCon: FC = () => {
+  return <RestaurantHomePre />;
+}
---
+++ frontend-react-app/src/features/restaurant/home/UI/Presentational/RestaurantHomePre.tsx
@@ -0,0 +1,12 @@
+import type { FC } from 'react';
+
+/**
+ * レストラン, ホーム画面のコンポーネント（Container）
+ * ここにコンポーネントのロジックを書いて、RestaurantHomePreに渡す
+ */
+
+export const RestaurantHomePre: FC = () => {
+  return <>
+    レストランホーム画面
+  </>;
+}

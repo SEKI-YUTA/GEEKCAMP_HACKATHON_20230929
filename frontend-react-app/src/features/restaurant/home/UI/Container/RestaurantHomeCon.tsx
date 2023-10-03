493078c76dc294240a36cf811cfc82376e0905a2
takeki <takekidanmkk@gmail.com>
2023-10-03 20:27:51 +0900

    仮の要素を入れた

--- frontend-react-app/src/features/restaurant/home/UI/Container/RestaurantHomeCon.tsx
+++ frontend-react-app/src/features/restaurant/home/UI/Container/RestaurantHomeCon.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 import { RestaurantHomePre } from '../Presentational/RestaurantHomePre';
+import { CategoryType } from '../../../../../application/@types/Category';
 
 /**
  * レストラン, ホーム画面のコンポーネント（Container）
@@ -7,5 +8,12 @@ import { RestaurantHomePre } from '../Presentational/RestaurantHomePre';
  */
 
 export const RestaurantHomeCon: FC = () => {
-  return <RestaurantHomePre />;
+  return <RestaurantHomePre
+    categoryList={[{ id: 1, name: 'カテゴリー1' }, { id: 2, name: 'カテゴリー2' }]}
+    menuItemList={[{ id: 1, name: 'メニュー1', price: 100, description: 'メニュー1の説明', photo_url: "", restaurant_id: 1, category: 'カテゴリー1' }, { id: 2, name: 'メニュー2', price: 200, description: 'メニュー2の説明', photo_url: "", restaurant_id: 1, category: 'カテゴリー2' }]}
+    selectedCategory={{} as CategoryType}
+    isLargerThan800={false}
+    isLargerThan1200={false}
+    onClickCategory={() => { }}
+  />;
 }
--- frontend-react-app/src/features/restaurant/home/UI/Presentational/RestaurantHomePre.tsx
+++ frontend-react-app/src/features/restaurant/home/UI/Presentational/RestaurantHomePre.tsx
@@ -1,12 +1,73 @@
 import type { FC } from 'react';
+import { Layout } from '../../../../../application/UI/Components/layout';
+import { MenuItem } from '../../../../owner/home/UI/Components/MenuItem';
+import { Category } from '../../../../owner/home/UI/Components/Category';
+import { Box, Grid, HStack } from '@chakra-ui/react';
+import type { MenuItemType } from '../../../../../application/@types/Menu';
+import type { CategoryType } from '../../../../../application/@types/Category';
 
-/**
- * レストラン, ホーム画面のコンポーネント（Container）
- * ここにコンポーネントのロジックを書いて、RestaurantHomePreに渡す
- */
+interface RestaurantHomePreProps {
+  /**
+   * 表示するメニューのリスト
+   */
+  menuItemList: MenuItemType[]
+  /**
+   * カテゴリーのリスト
+   */
+  categoryList: CategoryType[]
+  /**
+   * 選択されているカテゴリー
+   */
+  selectedCategory: CategoryType
+  /**
+   * 800px以上かどうか
+   */
+  isLargerThan800: boolean
+  /**
+   * 1200px以上かどうか
+   */
+  isLargerThan1200: boolean
+  /**
+   * カテゴリーを押したときの処理
+   * @param category 
+   * @returns 
+   */
+  onClickCategory: (category: CategoryType) => void
+}
 
-export const RestaurantHomePre: FC = () => {
-  return <>
-    レストランホーム画面
-  </>;
+export const RestaurantHomePre: FC<RestaurantHomePreProps> = ({
+  categoryList,
+  menuItemList,
+  selectedCategory,
+  isLargerThan800,
+  isLargerThan1200,
+  onClickCategory,
+}) => {
+  return (
+    <>
+      <Layout title='MaaS'>
+        <Box px={isLargerThan800 ? 12 : 5} pb={5}>
+          <HStack py={5}>
+            <HStack overflowX='auto' flex={4} spacing={1}>
+              {categoryList.map((category, index) => (
+                <Category key={index} category={category} isSelected={category.id === selectedCategory.id} isLargerThan1200={isLargerThan1200} onClick={() => onClickCategory(category)} />
+              ))}
+            </HStack>
+          </HStack>
+          <Box>
+            <Grid gridTemplateColumns={isLargerThan800 ? 'repeat(4, 1fr)' : 'repeat(2, 1fr)'} rowGap={5} columnGap={isLargerThan800 ? 10 : 5}>
+              {menuItemList.map((menuItem, index) => (
+                <MenuItem
+                  key={index}
+                  isLargerThan1200={isLargerThan1200}
+                  item={menuItem}
+                  onPress={() => { console.log('アイテム' + menuItem.id); }}
+                />
+              ))}
+            </Grid>
+          </Box>
+        </Box>
+      </Layout>
+    </>
+  );
 }

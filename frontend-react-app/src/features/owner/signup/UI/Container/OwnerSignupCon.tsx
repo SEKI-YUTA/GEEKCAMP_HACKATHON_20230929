9465b083145c5f44dcb71fb8488cdca57572ab73
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2023-10-04 23:53:08 +0900

    ラジオボタンの実装。 送信ボタンを押しても登録できない。

--- frontend-react-app/src/features/owner/signup/UI/Container/OwnerSignupCon.tsx
+++ frontend-react-app/src/features/owner/signup/UI/Container/OwnerSignupCon.tsx
@@ -1,7 +1,17 @@
-import type { FC, ChangeEvent } from 'react';
+import type { FC, ChangeEvent, FormEvent } from 'react';
 import { useState, useEffect } from 'react';
 
 import { OwnerSignupPre } from '../Presentational/OwnerSignupPre';
+
+export interface OwnerRestaurantCategoryType {
+  id: number
+  name: string
+}
+
+interface OwnerRestaurantCategoryResponse {
+  category_name: string
+  categories: OwnerRestaurantCategoryType[]
+}
 
 export const OwnerSignupCon: FC = () => {
 
@@ -11,6 +21,10 @@ export const OwnerSignupCon: FC = () => {
   const [phone_number, setPhone_number] = useState<string>('');
   const [address, setAddress] = useState<string>('');
   const [description, setDescription] = useState<string>('');
+  const [restaurantCategory, setRestaurantCategory] = useState<OwnerRestaurantCategoryType[]>([])
+  // const [category_id, setCategory_id] = useState<string>('');
+  const [selectedValue, setSelectedValue] = useState<string>('0');
+  const [errorMsg, setErrorMsg] = useState<number>(0)
 
   const handleOwnerEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     setOwnerEmail(e.target.value);
@@ -36,9 +50,64 @@ export const OwnerSignupCon: FC = () => {
     setDescription(e.target.value);
   };
 
+  const handleRadioGroupChange = (value: string) => {
+    setSelectedValue(value)
+  };
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (ownerEmail === '' || ownerEmail.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(1);
+
+      } else if (ownerPassword === '' || ownerPassword.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(2);
+
+      } else if (name === '' || name.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(3);
+
+      } else if (phone_number.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(4);
+
+      } else if (address.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(5);
+
+      } else if (description.split(' ').join('').split('　').join('') === '') {
+        setErrorMsg(6);
+
+      } else if (selectedValue === '') {
+        setErrorMsg(7);
+
+      }
+      
+      const responce = await fetch('http://localhost:8080/restaurants/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "email": ownerEmail,
+          "password": ownerPassword,
+          "name": name,
+          "phone_number": phone_number,
+          "address": address,
+          "description": description,
+          "category_id": selectedValue
+        })
+      });
+      console.log(responce);
+
+      // const json = await responce.json();
+      // if(json.)
+    } catch (error) {
+      setErrorMsg(8);
+    }
+  };
+
   const fetchRestaurantCategorys = async () => {
     const response = await fetch('http://localhost:8080/restaurants/categories');
-    const data = await response.json();
+    const data: OwnerRestaurantCategoryResponse = await response.json();
+    setRestaurantCategory(data.categories);
     console.log(data)
   };
 
@@ -53,12 +122,17 @@ export const OwnerSignupCon: FC = () => {
     phone_number={phone_number}
     address={address}
     description={description}
+    selectedValue={selectedValue}
     handleOwnerEmailChange={handleOwnerEmailChange}
     handleOwnerPasswordChange={handleOwnerPasswordChange}
     handleNameChange={handleSetNameChange}
     handlePhone_numberChange={handlePhone_numberChange}
     handleSetAddress={handleSetAddress}
     handleDescription={handleDescription}
+    handleRadioGroupChange={handleRadioGroupChange}
+    handleFormSubmit={handleFormSubmit}
+    restaurantCategory={restaurantCategory}
+    errorMsg={errorMsg}
   />;
 
 };
--- frontend-react-app/src/features/owner/signup/UI/Presentational/OwnerSignupPre.tsx
+++ frontend-react-app/src/features/owner/signup/UI/Presentational/OwnerSignupPre.tsx
@@ -1,4 +1,4 @@
-import type { FC, ChangeEvent } from 'react';
+import type { FC, ChangeEvent, FormEvent } from 'react';
 
 import {
   Box,
@@ -8,8 +8,16 @@ import {
   Stack,
   Button,
   useColorModeValue,
-  VStack
+  VStack,
+  CheckboxGroup,
+  Checkbox,
+  Radio,
+  RadioGroup,
+  Grid,
+  GridItem
 } from '@chakra-ui/react';
+
+import { OwnerRestaurantCategoryType } from '../Container/OwnerSignupCon';
 
 interface OwnerSignupProps {
   ownerEmail: string
@@ -18,12 +26,17 @@ interface OwnerSignupProps {
   phone_number: string
   address: string
   description: string
+  selectedValue: string
+  restaurantCategory: OwnerRestaurantCategoryType[]
+  errorMsg: number
   handleOwnerEmailChange: (e: ChangeEvent<HTMLInputElement>) => void
   handleOwnerPasswordChange: (e: ChangeEvent<HTMLInputElement>) => void
   handleNameChange: (e: ChangeEvent<HTMLInputElement>) => void
   handlePhone_numberChange: (e: ChangeEvent<HTMLInputElement>) => void
   handleSetAddress: (e: ChangeEvent<HTMLInputElement>) => void
   handleDescription: (e: ChangeEvent<HTMLInputElement>) => void
+  handleRadioGroupChange: (value: string) => void
+  handleFormSubmit: (e: FormEvent<HTMLFormElement>) => Promise<void>
   // category_id: number
 }
 
@@ -35,94 +48,100 @@ export const OwnerSignupPre: FC<OwnerSignupProps> = ({
   phone_number,
   address,
   description,
+  selectedValue,
   handleOwnerEmailChange,
   handleOwnerPasswordChange,
   handleNameChange,
   handlePhone_numberChange,
   handleSetAddress,
   handleDescription,
-
+  handleRadioGroupChange,
+  handleFormSubmit,
+  restaurantCategory,
   // category_id
 }) => {
 
   return (
-  // <Flex
-  //   minH={'100vh'}
-  //   align={'center'}
-  //   justify={'center'}
-  //   bg={useColorModeValue('gray.50', 'gray.800')}>
+    // <Flex
+    //   minH={'100vh'}
+    //   align={'center'}
+    //   justify={'center'}
+    //   bg={useColorModeValue('gray.50', 'gray.800')}>
 
     // <Box maxWidth={'1000px'} >
     <VStack spacing={8} mx="auto" maxW="2xl" py={12} px={6} align="stretch">
-      <Box
-        rounded="lg"
-        bg={useColorModeValue('white', 'gray.700')}
-        boxShadow="lg"
-        p={8}>
-        <Stack spacing={4}>
-          <FormControl id="email">
-            <FormLabel htmlFor="ownerEmail">メールアドレス</FormLabel>
-            <Input type="email" id="ownerEmail" placeholder="メールアドレスを入力" value={ownerEmail} onChange={handleOwnerEmailChange}/>
-            {/* <Input type="email" id="ownerEmail" placeholder="メールアドレスを入力" /> */}
-          </FormControl>
+      <form onSubmit={handleFormSubmit}>
+        <Box
+          rounded="lg"
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow="lg"
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel htmlFor="ownerEmail">メールアドレス</FormLabel>
+              <Input type="email" id="ownerEmail" placeholder="メールアドレスを入力" value={ownerEmail} onChange={handleOwnerEmailChange} />
+              {/* <Input type="email" id="ownerEmail" placeholder="メールアドレスを入力" /> */}
+            </FormControl>
 
-          <FormControl id="password">
-            <FormLabel htmlFor="ownerpassword">パスワード</FormLabel>
-            <Input type="password" id="ownerpassword" placeholder="パスワードを入力" value={ownerPassword} onChange={handleOwnerPasswordChange}/>
-            {/* <Input type="password" id="ownerpassword" placeholder="パスワードを入力" /> */}
-          </FormControl>
+            <FormControl id="password">
+              <FormLabel htmlFor="ownerpassword">パスワード</FormLabel>
+              <Input type="password" id="ownerpassword" placeholder="パスワードを入力" value={ownerPassword} onChange={handleOwnerPasswordChange} />
+              {/* <Input type="password" id="ownerpassword" placeholder="パスワードを入力" /> */}
+            </FormControl>
 
-          <FormControl id="name">
-            <FormLabel htmlFor="name">名前</FormLabel>
-            <Input type="text" id="name" placeholder="名前を入力" value={name} onChange={handleNameChange}/>
-            {/* <Input type="text" id="name" placeholder="名前を入力" /> */}
-          </FormControl>
+            <FormControl id="name">
+              <FormLabel htmlFor="name">名前</FormLabel>
+              <Input type="text" id="name" placeholder="名前を入力" value={name} onChange={handleNameChange} />
+              {/* <Input type="text" id="name" placeholder="名前を入力" /> */}
+            </FormControl>
 
-          <FormControl id="phone_number">
-            <FormLabel htmlFor="phone_number">電話番号</FormLabel>
-            <Input type="text" id="phone_number" placeholder="名前を入力" value={phone_number} onChange={handlePhone_numberChange}/>
-            {/* <Input type="text" id="phone_number" placeholder="名前を入力" /> */}
-          </FormControl>
+            <FormControl id="phone_number">
+              <FormLabel htmlFor="phone_number">電話番号</FormLabel>
+              <Input type="text" id="phone_number" placeholder="名前を入力" value={phone_number} onChange={handlePhone_numberChange} />
+              {/* <Input type="text" id="phone_number" placeholder="名前を入力" /> */}
+            </FormControl>
 
-          <FormControl id="address">
-            <FormLabel htmlFor="address">住所</FormLabel>
-            <Input type="text" id="address" placeholder="住所を入力" value={address} onChange={handleSetAddress}/>
-            {/* <Input type="text" id="address" placeholder="住所を入力" /> */}
-          </FormControl>
+            <FormControl id="address">
+              <FormLabel htmlFor="address">住所</FormLabel>
+              <Input type="text" id="address" placeholder="住所を入力" value={address} onChange={handleSetAddress} />
+              {/* <Input type="text" id="address" placeholder="住所を入力" /> */}
+            </FormControl>
 
-          <FormControl id="description">
-            <FormLabel htmlFor="description">お店の説明</FormLabel>
-            <Input type="text" id="description" placeholder="お店の説明を入力" value={description} onChange={handleDescription}/>
-            {/* <Input type="text" id="description" placeholder="お店の説明を入力" /> */}
-          </FormControl>
+            <FormControl id="description">
+              <FormLabel htmlFor="description">お店の説明</FormLabel>
+              <Input type="text" id="description" placeholder="お店の説明を入力" value={description} onChange={handleDescription} />
+            </FormControl>
 
-          {/* <FormControl id="category_id">
-            <FormLabel htmlFor="category_id">お店のカテゴリー</FormLabel>
-            <CheckboxGroup colorScheme='green' >
-              <Stack spacing={8} direction={'row'}>
-                <Checkbox borderColor={'gray'} value=''>居酒屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>カレー屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>ラーメン屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>中華料理屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>イタリアン</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>焼肉屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>韓国料理屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>洋食屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>和食屋</Checkbox>
-                <Checkbox borderColor={'gray'} value=''>その他</Checkbox>
-              </Stack>
-            </CheckboxGroup>
-          </FormControl> */}
-          <Button
-            bg="blue.400"
-            color="white"
-            _hover={{
-              bg: 'blue.500',
-            }}>
-            Sign in
-          </Button>
-        </Stack>
-      </Box>
+            <FormControl id="category_id">
+              <FormLabel htmlFor="category_id">お店のカテゴリー</FormLabel>
+              <RadioGroup colorScheme='green' value={selectedValue} onChange={handleRadioGroupChange}>
+                <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+                  {/* <Grid templateColumns={{ base: "repeat(5, 1fr)", md: "repeat(7, 1fr)", lg: "repeat(9, 1fr)" }} gap={6}> */}
+
+                  {
+                    restaurantCategory.map((item, index) => {
+                      return <>
+                        <GridItem w='100%' h='10' key={index}>
+                          <Radio borderColor={'gray'} value={index.toString()}>{item.name}</Radio>
+                        </GridItem>
+                      </>
+
+                    })
+                  }
+                </Grid>
+              </RadioGroup>
+            </FormControl>
+            <Button
+              bg="blue.400"
+              color="white"
+              _hover={{
+                bg: 'blue.500',
+              }} type='submit'>
+              Sign in
+            </Button>
+          </Stack>
+        </Box>
+      </form>
     </VStack>
     // </Box>
     // </Flex>

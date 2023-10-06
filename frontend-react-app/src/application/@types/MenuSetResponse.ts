62817ed4bb0c33e2a4347096c9427d9822af9d16
takeki <takekidanmkk@gmail.com>
2023-10-06 18:52:40 +0900

    予算から取得するときのレスポンスの型

---
+++ frontend-react-app/src/application/@types/MenuSetResponse.ts
@@ -0,0 +1,8 @@
+import { MenuItemType } from "./Menu";
+
+// GET /restaurants/:id/menus/yosan のレスポンスの型を定義する
+export type MenuSetResponse = {
+  status: string;
+  yosan: number;
+  menuSet: MenuItemType[];
+};

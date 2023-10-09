01dd1b27727a3a5b077157bdda5111f524f167f8
takeki <takekidanmkk@gmail.com>
2023-10-09 12:47:46 +0900

    ヘッダーをつけた

--- frontend-react-app/src/application/UI/Components/NotFound/NotFound.tsx
+++ frontend-react-app/src/application/UI/Components/NotFound/NotFound.tsx
@@ -1,10 +1,18 @@
 import { Box } from '@chakra-ui/react';
 import type { FC } from 'react';
+import { Layout } from '../layout';
 
 export const NotFound: FC = () => {
   return (
-    <Box textAlign='center' fontSize='2xl' fontWeight='bold' pt={10}>
+    <Layout title='MaaS'>
+      <Box textAlign='center' fontSize='2xl' fontWeight='bold' pt={10}
+        position='absolute'
+        top='50%'
+        left='50%'
+        transform='translate(-50%, -50%)'
+      >
       お探しのページは見つかりませんでした
-    </Box>
+      </Box>
+    </Layout>
   );
 };
